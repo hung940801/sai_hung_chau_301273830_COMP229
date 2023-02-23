4eab48c35020ff0c2940008d9dfdbe4aafcaca72
Cyrus Chau <hung940801@live.hk>
2023-02-23 00:00:54 -0500

    add production mongo db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,8 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contacts_store"
+    // Local
+    // "URI": "mongodb://localhost/business_contacts_store"
+    
+    // Production
+    "URI": "mongodb+srv://comp229_assign:BvdxMLkZKU2LPnm5@comp229assignment.uapwquy.mongodb.net/?retryWrites=true&w=majority"
 }
